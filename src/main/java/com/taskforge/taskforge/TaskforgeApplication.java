781e1e2258ced57bb70a7e5aff7093b0d3acf23a
VENKAT1931 <venkat31v@gmail.com>
2026-04-15 11:12:22 +0530

    fixed package scan

--- src/main/java/com/taskforge/taskforge/TaskforgeApplication.java
+++ src/main/java/com/taskforge/taskforge/TaskforgeApplication.java
@@ -3,7 +3,7 @@ package com.taskforge.taskforge;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "com.taskforge.taskforge")
 public class TaskforgeApplication {
 
 	public static void main(String[] args) {
